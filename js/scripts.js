aa4492d17b726f75021bca4f99ffeb4cb5e719ae
Rob Sameh <rob.sameh@gmail.com>
2022-01-28 16:08:18 -0800

    WIP: added question 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+function myFunction() {
+  let text = "Do you like to code?\nPress OK for yes or Cancel for no.";
+  if (confirm(text) == true) {
+    text = "You like to code!";
+  } else {
+    text = "You don't like to code!";
+  }
+  document.getElementById("demo").innerHTML = text;
+}
+
 $(document).ready(function() {
   $('input[type="radio"]').click(function() {
       let inputValue = $(this).attr("value");

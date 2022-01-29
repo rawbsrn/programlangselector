a0fd51087d55865aa625ebd755dfa8317a9bc4ad
Rob Sameh <rob.sameh@gmail.com>
2022-01-29 14:39:29 -0800

    WIP reading radio button inputs

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,9 @@ $(document).ready(function() {
   $("button#results").click(function(){
     $("#quiz").hide();
     const name1Input = $("input#name").val()
-    let langChoice = prompt("testprompt");
+    // let langChoice = prompt("testprompt");
+    let langChoice = $("input:radio[name=likeSnakes]:checked").val();
+
     $(".nameInput").text(name1Input);
     $(".langInput").text(langChoice);
     $("#quizResults").show();

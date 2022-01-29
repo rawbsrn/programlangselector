3af9e55ca7f75e3ecff3e1a01b936c3766f3900a
Rob Sameh <rob.sameh@gmail.com>
2022-01-28 18:29:19 -0800

    added 3 survey questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,3 @@
-function myFunction() {
-  let text = "Do you like to code?\nPress OK for yes or Cancel for no.";
-  if (confirm(text) == true) {
-    text = "You like to code!";
-  } else {
-    text = "You don't like to code!";
-  }
-  document.getElementById("demo").innerHTML = text;
-}
-
 $(document).ready(function() {
   $('input[type="radio"]').click(function() {
       let inputValue = $(this).attr("value");
